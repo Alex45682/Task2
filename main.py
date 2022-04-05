80d6becded7c9b7b1db572e246d36dfe7303e743
Alex45682 <alexandraalexeenko647@gmail.com>
2022-04-06 01:05:17 +0300

    small fix

--- main.py
+++ main.py
@@ -4,55 +4,81 @@ import os.path
 import datetime
 import logging
 
+lvl = {1: "INFO", 2: "DEBUG"}
 
-def menu():
-    lvl = {1: "INFO", 2: "DEBUG"}
+extensions = {
+    1: '.txt',
+    2: '.csv',
+    3: '.dsv'
+}
 
-    log_level = (input('Which log level you want to use: 1 - INFO, 2 - DEBUG'))
-    print("Your log level is: {}".format(lvl[int(log_level)]))
-    print("* * * *")
 
+def log_file_path_input():
     log_file_path = (input("Please provide a path for new log file (with extension '.log')"))
     print("Your path to to log file is: {}".format(log_file_path))
-    # example --> /Users/store/PycharmProjects/SecondTask/logs/new_log.log
-    print("* * * *")
 
-    open(log_file_path, "w+")
+    return log_file_path
 
-    logging.basicConfig(filename=log_file_path, level=logging.INFO if log_level == 1 else logging.DEBUG)
 
-    logging.info('Started')
+def log_level_input():
+    log_level = 0
 
-    logging.info('Log file with path {} is created'.format(log_file_path))
+    while log_level not in ['1', '2']:
+        log_level = (input('Which log level you want to use: 1 - INFO, 2 - DEBUG'))
 
-    logging.info('Log level: {} is selected'.format(lvl[int(log_level)]))
+    print("Your log level is: {}".format(lvl[int(log_level)]))
 
-    extensions = {
-        1: '.txt',
-        2: '.csv',
-        3: '.dsv'
-    }
+    return int(log_level)
 
+
+def file_extension_input():
     file_extension = int(input(
         'Files with what extension do you want to archive? 1 - "txt", 2 - "csv", 3 - "dsv": '))
     message = 'Files with extension: {} were selected for archiving'.format(extensions[file_extension])
 
     print(message)
     logging.info(message)
-    print("* * * *")
+    return file_extension
 
+
+def source_folder_input():
     source_folder = (input('Please provide a path for source folder'))
-    # example --> /Users/store/PycharmProjects/SecondTask/files
 
     logging.info('Source folder with path: {} is selected'.format(source_folder))
     print("Your path for source folder is: {}".format(source_folder))
-    print("* * * *")
+    return source_folder
 
+
+def output_file_path_input():
     output_path = (input('Please provide a path for output file '))
-    # example --> /Users/store/PycharmProjects/SecondTask
 
     print("Your path for output folder is: {}".format(output_path))
     logging.info('Output folder with path: {} is selected'.format(output_path))
+    return output_path
+
+
+def input_menu():
+    log_level = log_level_input()
+    print("* * * *")
+    log_file_path = log_file_path_input()  # example --> /Users/store/PycharmProjects/SecondTask/logs/new_log.log
+    print("* * * *")
+
+    open(log_file_path, "w+")
+
+    level = logging.INFO if log_level == 1 else logging.DEBUG
+    logging.basicConfig(filename=log_file_path, level=level)
+
+    logging.info('Started')
+    logging.info('Log file with path {} is created'.format(log_file_path))
+    logging.info('Log level: {} is selected'.format(lvl[int(log_level)]))
+
+    file_extension = file_extension_input()
+    print("* * * *")
+
+    source_folder = source_folder_input()  # example --> /Users/store/PycharmProjects/SecondTask/files
+    print("* * * *")
+
+    output_path = output_file_path_input()  # example --> /Users/store/PycharmProjects/SecondTask
     print("* * * *")
 
     return {"source_folder": source_folder, "output_path": output_path, "extension": extensions[file_extension]}
@@ -69,7 +95,7 @@ def zip_files(source_folder, output_path, extension):
     files_with_needed_extension = []
     for (dirpath, dirnames, filenames) in walk(source_folder):
         f.extend(filenames)
-        break
+
     for file in f:
         if extension in file:
             files_with_needed_extension.append(file)
@@ -78,16 +104,12 @@ def zip_files(source_folder, output_path, extension):
     zipObj = ZipFile(file_name.format(output_path + "/" + file_name), 'w')
 
     for i in files_with_needed_extension:
-         zipObj.write(os.path.abspath(source_folder + "/" + i))
+        zipObj.write(os.path.abspath(source_folder + "/" + i))
 
     logging.info("Zip file with name {} is created".format(file_name))
-    zipObj.close()
 
     logging.info("Finished")
 
-props = menu()
-zip_files(props['source_folder'], props['output_path'], props['extension'])
 
-
-
-
+params = input_menu()
+zip_files(params['source_folder'], params['output_path'], params['extension'])
